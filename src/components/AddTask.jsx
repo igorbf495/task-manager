90efcb6c9c02a0e41b5088833b6175b2fad429a3
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-08 16:00:38 -0300

    feat: state for add task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,28 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
   return (
- 
-      <div className="bg-slate-200 rounded-md shadow space-y-4 p-6 flex flex-col">
-        <input type="text" placeholder="Title" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
-        <input type="text" placeholder="Description"className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
-        <button className=" bg-slate-500 px-4 py-2 text-white ">Add</button>
-      </div>
-    
+    <div className="bg-slate-200 rounded-md shadow space-y-4 p-6 flex flex-col">
+      <input
+        type="text"
+        placeholder="Title"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button onClick={() => onAddTaskSubmit(title, description)} className=" bg-slate-500 px-4 py-2 text-white ">Add</button>
+    </div>
   );
 }
 
