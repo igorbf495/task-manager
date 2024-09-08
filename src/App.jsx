308b6990a75b04042be48398533c2a5ce155dc19
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-08 02:30:52 -0300

    feat: function onTaskClick

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,19 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // preciso atualizar essa tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // não preciso atualizar essa tarefa
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <>
       <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
@@ -30,7 +43,7 @@ function App() {
           <h1 className="text-3xl text-slate-100 font-bold text-center">
             Gerenciador de Tarefas
           </h1>
-          <Tasks tasks={tasks} />
+          <Tasks tasks={tasks} onTaskClick={onTaskClick} />
         </div>
       </div>
     </>
