a4657f3ba07b3156fc271fa451122d184f2c566e
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-08 02:39:09 -0300

    using funcion onTaskClick as prop

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,8 +6,12 @@ function Tasks(props) {
       <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
         {props.tasks.map((task) => (
           <li key={task.id} className="flex gap-2">
-            <button className="w-full text-left bg-slate-400 text-white p-2 rounded-md">
+            <button
+              onClick={() => props.onTaskClick(task.id)}
+              className="w-full text-left bg-slate-400 text-white p-2 rounded-md"
+            >
               {task.title}
+              {task.isCompleted ? "COMPLETE" : "IMCOMPLETE"}
             </button>
 
             <button className="bg-slate-400 p-2 rounded-md text-white">
