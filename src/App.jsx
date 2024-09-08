0fbed3372814c90f133dd59d1ebd97b8d906fd60
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-08 02:56:11 -0300

    feat: function onDeleteTaskClick

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,11 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return (
     <>
       <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
@@ -43,7 +48,11 @@ function App() {
           <h1 className="text-3xl text-slate-100 font-bold text-center">
             Gerenciador de Tarefas
           </h1>
-          <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+          <Tasks
+            tasks={tasks}
+            onTaskClick={onTaskClick}
+            onDeleteTaskClick={onDeleteTaskClick}
+          />
         </div>
       </div>
     </>
