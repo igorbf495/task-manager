f8cf45bfff7d6c156797feef2aa3f55f6c423afc
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-08 15:13:02 -0300

    feat: new component AddTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -44,10 +45,11 @@ function App() {
   return (
     <>
       <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-        <div className="w-[500px]">
+        <div className="w-[500px] space-y-4">
           <h1 className="text-3xl text-slate-100 font-bold text-center">
             Gerenciador de Tarefas
           </h1>
+          <AddTask />
           <Tasks
             tasks={tasks}
             onTaskClick={onTaskClick}
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,15 @@
+import { Input } from "postcss";
+
 function AddTask() {
-    <h1>Add Task</h1>
+  return (
+ 
+      <div className="bg-slate-200 rounded-md shadow space-y-4 p-6 flex flex-col">
+        <input type="text" placeholder="Title" className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
+        <input type="text" placeholder="Description"className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" />
+        <button className=" bg-slate-500 px-4 py-2 text-white ">Add</button>
+      </div>
+    
+  );
 }
 
-export default AddTask
+export default AddTask;
