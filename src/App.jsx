af4b77eb86ea78a07cc19223074a13bdd0d2a86d
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-08 01:33:41 -0300

    feat: create state task

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
- 
-
+  const [tasks, setTsks] = useState([
+    {
+      id: 1,
+      title: "Estudar Typescript",
+      description: "nunca parar de estudar",
+      isComplited: false,
+    },
+  ]);
   return (
     <>
-      <h1 className="text-red-500" >Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask />
-
+      <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+        <div className="w-[500px]">
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Gerenciador de Tarefas
+          </h1>
+        </div>
+      </div>
     </>
   );
 }
