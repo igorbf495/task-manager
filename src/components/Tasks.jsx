7988151865bf2e9f910c332a8e6d824accff3cb1
igorbf495 <igorcarvalhofelipe495@gmail.com>
2024-09-08 21:00:58 -0300

    using params after captured

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,15 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+
+    const navigate = useNavigate()
+
+    function onSeeDatailsClick(task){
+        navigate(`/task?title=${task.title}&description=${task.description}`)
+    }
+
+
   return (
     <>
       <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
@@ -13,12 +22,12 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
               {task.title}
             </button>
 
-            <button className="bg-slate-400 p-2 rounded-md text-white">
+            <button onClick={() => onSeeDatailsClick(task)} className="bg-slate-400 p-2 rounded-md text-white">
               <ChevronRightIcon />
             </button>
 
-            <button className="bg-slate-400 p-2 rounded-md text-white">
-              <TrashIcon onClick={() => onDeleteTaskClick(task.id)} />
+            <button onClick={() => onDeleteTaskClick(task.id)} className="bg-slate-400 p-2 rounded-md text-white">
+              <TrashIcon  />
             </button>
           </li>
         ))}
